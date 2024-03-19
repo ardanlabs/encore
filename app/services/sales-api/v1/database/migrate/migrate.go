356e82d9f83a4aae1ee00572dde917b42d98bea7
William Kennedy <bill@ardanlabs.com>
2024-03-19 17:25:03 -0400

    have unit tests working, need help on integration

---
+++ app/services/sales-api/v1/database/migrate/migrate.go
@@ -0,0 +1,84 @@
+package migrate
+
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"errors"
+	"fmt"
+
+	"github.com/ardanlabs/encore/business/data/sqldb"
+	"github.com/jmoiron/sqlx"
+)
+
+//go:embed migrations/1_migrate.up.sql
+var migrationDoc string
+
+// Migrate will run the migration files.
+func Migrate(ctx context.Context, db *sqlx.DB) (err error) {
+	if err := sqldb.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if errTx := tx.Rollback(); errTx != nil {
+			if errors.Is(errTx, sql.ErrTxDone) {
+				return
+			}
+
+			err = fmt.Errorf("rollback: %w", errTx)
+			return
+		}
+	}()
+
+	if _, err := tx.Exec(migrationDoc); err != nil {
+		return fmt.Errorf("exec: %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("commit: %w", err)
+	}
+
+	return nil
+}
+
+//go:embed seeds/seed.sql
+var seedDoc string
+
+// Seed will insert data needed for a new database.
+func Seed(ctx context.Context, db *sqlx.DB) (err error) {
+	if err := sqldb.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if errTx := tx.Rollback(); errTx != nil {
+			if errors.Is(errTx, sql.ErrTxDone) {
+				return
+			}
+
+			err = fmt.Errorf("rollback: %w", errTx)
+			return
+		}
+	}()
+
+	if _, err := tx.Exec(seedDoc); err != nil {
+		return fmt.Errorf("exec: %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("commit: %w", err)
+	}
+
+	return nil
+}
