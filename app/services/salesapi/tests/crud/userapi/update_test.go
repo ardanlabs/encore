7d17e4c05f1859c94d200f104a98d26dd62260f5
William Kennedy <bill@ardanlabs.com>
2024-03-27 07:41:11 -0500

    missed change with date updated

--- app/services/salesapi/tests/crud/userapi/update_test.go
+++ app/services/salesapi/tests/crud/userapi/update_test.go
@@ -25,7 +25,7 @@ func userUpdateOk(sd dbtest.SeedData) []dbtest.AppTable {
 				Department:  "IT",
 				Enabled:     true,
 				DateCreated: sd.Users[0].DateCreated.Format(time.RFC3339),
-				DateUpdated: sd.Users[0].DateUpdated.Format(time.RFC3339),
+				DateUpdated: sd.Users[0].DateCreated.Format(time.RFC3339),
 			},
 			ExcFunc: func(ctx context.Context) any {
 				app := userapi.AppUpdateUser{
